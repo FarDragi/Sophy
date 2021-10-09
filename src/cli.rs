6477ccb90700280e03962a6c96dffcd9d387ca99
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2021-10-09 17:30:34 -0300

    Feat: Implement clap for new arg sistem

---
+++ src/cli.rs
@@ -0,0 +1,19 @@
+use clap::{AppSettings, Clap};
+
+use crate::config::Config;
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Args {
+    #[clap(short, long)]
+    update_commands: bool,
+}
+
+impl Args {
+    pub fn aplay_configs(&self, config: &mut Config) {
+        if self.update_commands {
+            debug!("Update commands");
+            config.update_commands();
+        }
+    }
+}
