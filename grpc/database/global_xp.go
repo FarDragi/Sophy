edae0bd83e460c7a3f713c3d00a0a9c55aa7ea61
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-03-06 16:33:39 -0300

    Retornando os erros

--- grpc/database/global_xp.go
+++ grpc/database/global_xp.go
@@ -4,17 +4,21 @@ import "time"
 
 func GlobalXpAddOrCreate(discordId string, count int64) (GlobalXp, error) {
 	var globalXp GlobalXp
-	xp := Connection.Where(GlobalXp{
+	result := Connection.Where(GlobalXp{
 		DiscordId: discordId,
 	}).FirstOrCreate(&globalXp)
 
-	if xp.Error != nil {
-		return GlobalXp{}, xp.Error
+	if result.Error != nil {
+		return GlobalXp{}, result.Error
 	}
 
 	if (globalXp.UpdatedAt + 300) < int(time.Now().Unix()) {
 		globalXp.Progress += 1
-		Connection.Save(&globalXp)
+		result := Connection.Save(&globalXp)
+
+		if result.Error != nil {
+			return GlobalXp{}, result.Error
+		}
 	}
 
 	return globalXp, nil
@@ -23,9 +27,7 @@ func GlobalXpAddOrCreate(discordId string, count int64) (GlobalXp, error) {
 func (g GlobalXp) LevelUp(newLevel int32, newProgress int64) (GlobalXp, error) {
 	g.Level = newLevel
 	g.Progress = newProgress
-	result := Connection.Where(GlobalXp{
-		DiscordId: g.DiscordId,
-	}).Updates(&g)
+	result := Connection.Save(&g)
 
 	if result.Error != nil {
 		return GlobalXp{}, result.Error
