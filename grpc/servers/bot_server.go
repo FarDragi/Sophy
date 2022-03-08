e934a881f6719f9fb8969f2cfa9c60af11d0b39e
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-03-08 15:22:58 -0300

    Corrigindo o grpc

--- grpc/servers/bot_server.go
+++ grpc/servers/bot_server.go
@@ -4,41 +4,41 @@ import (
 	"context"
 
 	"fardragi/sophy/grpc/database"
-	"fardragi/sophy/grpc/pb/xp"
+	"fardragi/sophy/grpc/pb"
 	"fardragi/sophy/grpc/utils"
 
 	"google.golang.org/grpc"
 )
 
 func RegisterXpServer(server *grpc.Server) {
-	xp.RegisterXpServer(server, &XpServer{})
+	pb.RegisterBotServer(server, &BotServer{})
 }
 
-type XpServer struct {
-	xp.UnimplementedXpServer
+type BotServer struct {
+	pb.UnimplementedBotServer
 }
 
-func (s *XpServer) AddUserGlobalXp(ctx context.Context, req *xp.AddGlobalXp) (*xp.LevelUp, error) {
+func (s *BotServer) AddUserGlobalXp(ctx context.Context, req *pb.AddGlobalXp) (*pb.LevelUp, error) {
 	level, err := database.GlobalXpAddOrCreate(req.GetDiscordId(), req.GetCount())
 
 	if err != nil {
-		return &xp.LevelUp{}, err
+		return &pb.LevelUp{}, err
 	}
 
 	if newLevel, newProgress, levelUp := utils.IsLevelUp(level.Level, level.Progress); levelUp {
 		level, err := level.LevelUp(newLevel, newProgress)
 
 		if err != nil {
-			return &xp.LevelUp{}, err
+			return &pb.LevelUp{}, err
 		}
 
-		return &xp.LevelUp{
+		return &pb.LevelUp{
 			NewLevel: level.Level,
 			LevelUp:  true,
 		}, nil
 	}
 
-	return &xp.LevelUp{
+	return &pb.LevelUp{
 		LevelUp: false,
 	}, nil
 }
