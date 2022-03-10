991ac93ddbf9714bb171c680e2c9c32164516184
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-03-09 21:01:40 -0300

    Rename server

--- grpc/servers/sophy_server.go
+++ grpc/servers/sophy_server.go
@@ -10,35 +10,35 @@ import (
 	"google.golang.org/grpc"
 )
 
-func RegisterXpServer(server *grpc.Server) {
-	pb.RegisterBotServer(server, &BotServer{})
+func RegisterSophyServer(server *grpc.Server) {
+	pb.RegisterSophyServer(server, &BotServer{})
 }
 
 type BotServer struct {
-	pb.UnimplementedBotServer
+	pb.UnimplementedSophyServer
 }
 
-func (s *BotServer) AddUserGlobalXp(ctx context.Context, req *pb.AddGlobalXp) (*pb.LevelUp, error) {
-	level, err := database.GlobalXpAddOrCreate(req.GetDiscordId(), req.GetCount())
+func (s *BotServer) AddUserGlobalXp(ctx context.Context, req *pb.GlobalXpRequest) (*pb.GlobalXpResponse, error) {
+	level, err := database.GlobalXpAddOrCreate(req.GetDiscordId(), 1)
 
 	if err != nil {
-		return &pb.LevelUp{}, err
+		return &pb.GlobalXpResponse{}, err
 	}
 
 	if newLevel, newProgress, levelUp := utils.IsLevelUp(level.Level, level.Progress); levelUp {
 		level, err := level.LevelUp(newLevel, newProgress)
 
 		if err != nil {
-			return &pb.LevelUp{}, err
+			return &pb.GlobalXpResponse{}, err
 		}
 
-		return &pb.LevelUp{
+		return &pb.GlobalXpResponse{
 			NewLevel: level.Level,
 			LevelUp:  true,
 		}, nil
 	}
 
-	return &pb.LevelUp{
+	return &pb.GlobalXpResponse{
 		LevelUp: false,
 	}, nil
 }
